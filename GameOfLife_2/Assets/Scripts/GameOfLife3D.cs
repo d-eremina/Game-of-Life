7ec72129f24969cc14fa7398ecdb52e71e783ea7
Daria Eremina <dariaeremina@MacBook-Pro-Daria.local>
2020-05-04 17:12:02 +0300

    drawing in both modes works

--- GameOfLife_2/Assets/Scripts/GameOfLife3D.cs
+++ GameOfLife_2/Assets/Scripts/GameOfLife3D.cs
@@ -5,19 +5,84 @@ using UnityEngine;
 public class GameOfLife3D : GameOfLife2D
 {
     private GameObject[,,] grid;
-    private int width3D = 24;
-    private int height3D = 24;
-    private int depth = 24;
+    public int width3D = 24;
+    public int height3D = 24;
+    public int depth = 24;
+
+    public GameObject selectedCell;
+    public float cameraDistancePlacement = 10.0f;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         grid = new GameObject[width3D, height3D, depth];
+        selectedCell = Instantiate(selectedCell);
+        selectedCell.transform.SetParent(gameBoard);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlaying)
+        {
+
+        }
+        else
+        {
+            Vector3 pos = gameCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraDistancePlacement));
+            Vector3Int boardPos = new Vector3Int(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y), Mathf.RoundToInt(pos.z));
+            selectedCell.transform.position = boardPos;
+            if (boardPos.x >= 0 && boardPos.x <= width && boardPos.y >= 0 && boardPos.y <= height && boardPos.z >= 0 && boardPos.z <= depth)
+            {
+                selectedCell.SetActive(true);
+            }
+            else
+            {
+                selectedCell.SetActive(false);
+            }
+            bool mouseClicked = Input.GetMouseButtonDown(0);
+            if (mouseClicked)
+            {
+                ChangeCell(boardPos);
+            }
+        }
+    }
+
+    private void ChangeCell(Vector3Int cellPos)
+    {
+        try
+        {
+            GameObject cell = grid[cellPos.x, cellPos.y, cellPos.z];
+            if (cell == null)
+            {
+                CreateCell(cellPos);
+            }
+            else
+            {
+                DestroyCell(cellPos);
+            }
+        }
+        catch
+        {
+            return;
+        }
+    }
+
+    private void CreateCell(Vector3Int cellPos)
+    {
+        GameObject newCell = Instantiate(cellPrefab);
+        newCell.transform.SetParent(gameBoard);
+        newCell.transform.position = cellPos;
+        grid[cellPos.x, cellPos.y, cellPos.z] = newCell;
+    }
+
+    private void DestroyCell(Vector3Int cellPos)
+    {
+        GameObject deadCell = grid[cellPos.x, cellPos.y, cellPos.z];
+        if (deadCell != null)
+        {
+            Destroy(deadCell);
+        }
+        grid[cellPos.x, cellPos.y, cellPos.z] = null;
     }
 }
