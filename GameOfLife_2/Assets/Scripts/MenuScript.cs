87420a093c0c501aa8ac24519951f82ce51c4dbc
Daria Eremina <dariaeremina@MacBook-Pro-Daria.local>
2020-05-10 18:56:14 +0300

    main menu added

---
+++ GameOfLife_2/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    public void QuitGame() => Application.Quit();
+
+    public void ShowInfo()
+    {
+
+    }
+}
